3caf360a07fdca9b059eac9f9ea80bec476151f3
arex <e.fan.arex@gmail.com>
2017-09-18 22:01:39 +0800

    Vue2学习

---
+++ vue-todos/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="(todo,index) in todos"
+          :id="index">
+        <label>{{index + 1}}.{{ todo.value }}</label>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        title: 'vue-todos',
+        todos: [
+          {value: "阅读一本关于前端开发的书", done: false},
+          {value: "补充代码", done: true},
+          {value: "写心得", done: false}
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
