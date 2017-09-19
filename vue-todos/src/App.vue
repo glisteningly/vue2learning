fcec1e5ee3d822a28222fe80f1a8a55e97d7ff9f
arex <e.fan.arex@gmail.com>
2017-09-19 22:03:19 +0800

    filter 加入

--- vue-todos/src/App.vue
+++ vue-todos/src/App.vue
@@ -3,8 +3,10 @@
     <h1>{{ title }}</h1>
     <ul class="todos">
       <li v-for="(todo,index) in todos"
+          :class="{'checked': todo.done}"
           :id="index">
         <label>{{index + 1}}.{{ todo.value }}</label>
+        <time>{{todo.created | date}}</time>
       </li>
     </ul>
   </div>
@@ -13,16 +15,26 @@
 <script>
   import './assets/todos.less'
   import './assets/site.less'
+  import moment from 'moment'
+  import 'moment/locale/zh-cn'
+
+  moment.locale('zh-cn')
+
   export default {
     name: 'app',
     data() {
       return {
         title: 'vue-todos',
         todos: [
-          {value: "阅读一本关于前端开发的书", done: false},
-          {value: "补充代码", done: true},
-          {value: "写心得", done: false}
+          {value: "阅读一本关于前端开发的书", done: false, created: Date.now()},
+          {value: "补充代码", done: true, created: Date.now() + 300000},
+          {value: "写心得", done: false, created: Date.now() - 300000}
         ]
+      }
+    },
+    filters: {
+      date(val) {
+        return moment(val).calendar()
       }
     }
   }
