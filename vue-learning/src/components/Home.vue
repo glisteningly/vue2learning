f2e294b26cf58fed2cfeeaff51d3d53f4e81ca18
arex <e.fan.arex@gmail.com>
2017-10-15 21:18:45 +0800

    router学习使用

---
+++ vue-learning/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><h1>{{ msg }}</h1></div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        msg: 'Welcome to  Home'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
