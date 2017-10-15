d2cff13ba560eb99012fbd1de0f8d03f886aa323
arex <e.fan.arex@gmail.com>
2017-10-15 21:17:13 +0800

    router学习使用

---
+++ vue-learning/src/components/Explore.vue
@@ -0,0 +1,18 @@
+<template>
+  <div><h1>{{ msg }}</h1></div>
+</template>
+
+<script>
+  export default {
+    name: 'Explore',
+    data() {
+      return {
+        msg: 'Welcome to Explore'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
